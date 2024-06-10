c6c384f6e166fdedecbff1c12bc2b80b23d6cc60
Luís Costa <luis.pedro10@live.com.pt>
2024-06-10 18:19:22 +0100

    Formatting

--- app/chapters/layout.tsx
+++ app/chapters/layout.tsx
@@ -8,23 +8,23 @@ export default function ChaptersLayout({
 }>) {
   // Example progress value
   const progress = 1; // This should be dynamic based on actual progress
-return (
+  return (
     <main>
-        <div className="flex items-center justify-between px-10 p-4 bg-gray-800 text-white">
-            <Link href={"/"}>
-                <HomeIcon className="w-6 h-6" />
-            </Link>
+      <div className="flex items-center justify-between px-10 p-4 bg-gray-800 text-white">
+        <Link href={"/"}>
+          <HomeIcon className="w-6 h-6" />
+        </Link>
 
-            <div className="w-full mx-4 bg-gray-700 rounded-full h-2.5 dark:bg-gray-700 w-3/12">
-                <div
-                    className="bg-blue-600 h-2.5 rounded-full"
-                    style={{ width: `${progress}%` }}
-                ></div>
-            </div>
-
-            <TrophyIcon className="w-6 h-6" />
+        <div className="w-full mx-4 bg-gray-700 rounded-full h-2.5 dark:bg-gray-700 w-3/12">
+          <div
+            className="bg-blue-600 h-2.5 rounded-full"
+            style={{ width: `${progress}%` }}
+          ></div>
         </div>
-        {children}
+
+        <TrophyIcon className="w-6 h-6" />
+      </div>
+      {children}
     </main>
-);
+  );
 }
