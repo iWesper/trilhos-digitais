cb15a78026189068398a80d9b9ef7f026c4841b2
T0mas Sousa <tomasrafael@ua.pt>
2024-06-11 18:09:19 +0100

    Update image loading and page layouts for Register component

--- components/register/Register.jsx
+++ components/register/Register.jsx
@@ -153,8 +153,8 @@ export const Auth = () => {
 
     return(
       <div>
-        {Render === true ? (
-        <div className="w-full h-screen lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+        {Render ? (
+        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
                 <div className="flex items-center justify-center py-12">
                   <div className="mx-auto grid w-[350px] gap-6">
                     <div className="grid gap-2 text-center">
@@ -211,7 +211,6 @@ export const Auth = () => {
                     width="1920"
                     height="1080"
                     className="h-screen w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                    priority={true}
                   />
                 </div>
                 </div>
@@ -219,7 +218,7 @@ export const Auth = () => {
               </div>
 
       ): (
-        Render === false && <Login />
+        <Login />
       )}
       </div>
   );
