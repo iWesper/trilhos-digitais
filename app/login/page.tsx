51ff7c081a86f7d1c88a872d0e8c41960bb4e1f1
T0mas Sousa <tomasrafael@ua.pt>
2024-06-11 18:00:56 +0100

    Remove login and register page components
    This commit removes the login and register page components, as they are no longer needed in the application. These components were previously deleted in separate commits, but this commit consolidates the removal of both components.
    
    Refs: #123

--- app/login/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import Login from '../../components/login/Login';
-
-export default function LoginPage() {
-    return <Login />;
-}
--- app/register/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import {Auth} from '../../components/register/Register';
-
-export default function RegisterPage() {
-    return <Auth />;
-}
