df970ab5e9a55070bf4bb1e604c6eb39c90724de
T0mas Sousa <tomasrafael@ua.pt>
2024-06-12 16:28:17 +0100

    refactor: Update Comboio component to use conditional rendering for image link

--- app/badges/page.tsx
+++ app/badges/page.tsx
@@ -1,12 +1,62 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { HomeIcon, TrophyIcon } from "@heroicons/react/24/solid";
 import Navbar from "@/components/homepage/Navbar";
+import { useEffect,useState } from "react";
+import { auth } from "../../backend/config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import Login from "@/components/login/Login";
+import Lottie from "lottie-react";
+import animationData from "@/public/animations/loading_animation.json";
+
 
 export default function Badges() {
+
+   //USER ID
+  const [UserId, setUserId] = useState<string | null>(null);
+
+  //LOADING
+  const [loading, setLoading] = useState<boolean>(true);
+
+   //VAI BUSCAR O USER ID QUANDO MONTA
+   useEffect(() => {
+    //SAVE USER
+    onAuthStateChanged(auth, (currentUser) => {
+
+      if (currentUser) {
+        //SAVE
+        setUserId(currentUser.uid);
+
+        if (UserId) {
+          return;
+        }
+      } else {
+        setUserId(null);
+      }
+
+      //ACABA O LOAD
+      setLoading(false);
+    });
+  }, [UserId]);
+
+  //SE ESTIVER A CARREGAR
+  if (loading) {
+    return (
+    <div className="h-screen w-screen flex justify-center items-center">
+          <Lottie animationData={animationData} className="bg-foreground h-20 w-20 " />
+    </div>
+  )
+  }
+
   return (
-    <main>
-      <Navbar />
-    </main>
+    
+    UserId ? (
+      <main>
+        <Navbar />
+      </main>
+    ) : (
+      <Login />
+    )
   );
 }
--- components/chapters/chapter1/Chapter1Page1.tsx
+++ components/chapters/chapter1/Chapter1Page1.tsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { useEffect,useState } from "react";
+import { auth } from "../../../backend/config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import Login from "@/components/login/Login";
+import Lottie from "lottie-react";
+import animationData from "@/public/animations/loading_animation.json";
 
 export default function Chapter1Page1() {
+   //USER ID
+   const [UserId, setUserId] = useState<string | null>(null);
+
+   //LOADING
+   const [loading, setLoading] = useState<boolean>(true);
+
+   //VAI BUSCAR O USER ID QUANDO MONTA
+   useEffect(() => {
+    //SAVE USER
+    onAuthStateChanged(auth, (currentUser) => {
+
+      if (currentUser) {
+        //SAVE
+        setUserId(currentUser.uid);
+
+        if (UserId) {
+          return;
+        }
+      } else {
+        setUserId(null);
+      }
+
+      //ACABA O LOAD
+      setLoading(false);
+    });
+  }, [UserId]);
+
+  //SE ESTIVER A CARREGAR
+  if (loading) {
+    return (
+    <div className="h-screen w-screen flex justify-center items-center">
+          <Lottie animationData={animationData} className="bg-foreground h-20 w-20 " />
+    </div>
+  )
+  }
+
   return (
-    <div>
+    UserId ? (
+      <div>
       <h1>Capítulo 1 - Página 1</h1>
     </div>
+      
+    ) : (
+      <Login />
+    )
+    
   );
 }
