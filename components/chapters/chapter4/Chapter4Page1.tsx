36d43babaa30e6a2555ebfb98121d5994f07f884
T0mas Sousa <tomasrafael@ua.pt>
2024-06-12 16:29:00 +0100

    refactor: Update Chapter4Page1 component to include user authentication and loading animation
    This commit refactors the Chapter4Page1 component to include user authentication using Firebase and a loading animation. It adds the necessary imports and hooks for user authentication, sets up the loading state, and fetches the user ID when the component mounts. The loading animation is displayed while the user ID is being fetched. This change improves the functionality and user experience of the page.
    
    Refs: #123

--- components/chapters/chapter4/Chapter4Page1.tsx
+++ components/chapters/chapter4/Chapter4Page1.tsx
@@ -1,9 +1,59 @@
 import React from "react";
+import { useEffect,useState } from "react";
+import { auth } from "../../../backend/config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import Login from "@/components/login/Login";
+import Lottie from "lottie-react";
+import animationData from "@/public/animations/loading_animation.json";
+
 
 export default function Chapter4Page1() {
-  return (
+  //USER ID
+  const [UserId, setUserId] = useState<string | null>(null);
+
+  //LOADING
+  const [loading, setLoading] = useState<boolean>(true);
+
+  //VAI BUSCAR O USER ID QUANDO MONTA
+  useEffect(() => {
+   //SAVE USER
+   onAuthStateChanged(auth, (currentUser) => {
+
+     if (currentUser) {
+       //SAVE
+       setUserId(currentUser.uid);
+
+       if (UserId) {
+         return;
+       }
+     } else {
+       setUserId(null);
+     }
+
+     //ACABA O LOAD
+     setLoading(false);
+   });
+ }, [UserId]);
+
+ //SE ESTIVER A CARREGAR
+ if (loading) {
+   return (
+   <div className="h-screen w-screen flex justify-center items-center">
+         <Lottie animationData={animationData} className="bg-foreground h-20 w-20 " />
+   </div>
+ )
+ }
+
+
+ return (
+  UserId ? (
     <div>
-      <h1>Capítulo 4 - Página 1</h1>
-    </div>
-  );
+    <h1>Capítulo 4 - Página 1</h1>
+  </div>
+    
+  ) : (
+    <Login />
+  )
+  
+);
 }
