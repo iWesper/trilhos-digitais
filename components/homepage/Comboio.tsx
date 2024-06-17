4ce9130f0825282e46b7bb7c88288ebd0ef6dedf
Luís Costa <luis.pedro10@live.com.pt>
2024-06-17 13:23:12 +0100

    refactor: Update Comboio component to remove unnecessary responsive classes
    This commit updates the Comboio component to remove the unnecessary responsive classes in the CarouselItem element. The md:basis-1/2 and lg:basis-1/3 classes are no longer needed and have been removed. This change simplifies the code and improves the performance of the component.
    
    Refs: #123

--- components/homepage/Comboio.tsx
+++ components/homepage/Comboio.tsx
@@ -51,7 +51,7 @@ const comboio = [
     <Carousel className="w-full h-full">
       <CarouselContent>
         {comboio.map((carruagem) => (
-            <CarouselItem key={carruagem.id} className="md:basis-1/2 lg:basis-1/3">
+            <CarouselItem key={carruagem.id} className="md:basis-1/2">
               <Card className="h-full w-full">
                 <CardContent className="flex items-center justify-center h-full w-full">
                 {carruagem.id === 0 ? (
