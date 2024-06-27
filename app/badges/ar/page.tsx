5fa82631c9f494b02cfee167a6c3bfdbaf501401
Luís Costa <luis.pedro10@live.com.pt>
2024-06-27 22:23:14 +0100

    refactor: Update AR.js library to latest version

--- app/badges/ar/page.tsx
+++ app/badges/ar/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
 import { Canvas, useLoader } from "@react-three/fiber";
+import Head from "next/head";
 import Bauhaus from "@/public/models/bauhaus/Bauhaus";
+import { Environment, OrbitControls } from "@react-three/drei";
 // import "@/public/models/bauhaus/bauhaus.glb";
 
 const AR = () => {
@@ -11,7 +13,7 @@ const AR = () => {
     // Dynamically load AR.js script
     const script = document.createElement("script");
     script.src =
-      "https://cdn.jsdelivr.net/npm/@ar-js-org/ar.js@3.3.2/aframe/build/aframe-ar.js";
+      "https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js";
     document.head.appendChild(script);
 
     // Cleanup function to remove script
@@ -26,18 +28,21 @@ const AR = () => {
 
   return (
     <>
+      <Head>
+        <title>AR Experience</title>
+      </Head>
       {/* AR.js scene setup */}
       <a-scene className="w-full h-full" embedded arjs="sourceType: webcam;">
         <a-marker preset="hiro" onMarkerFound={() => loadModels()}>
           <a-entity position="0 0 0" />{" "}
           {isMarkerFound && (
-                <Canvas>
-                  <ambientLight />
-                  <pointLight position={[10, 10, 10]} />
-                  {/* Render the model when the marker is found */}
-                  <Bauhaus />
-                </Canvas>
-              )}
+            <Canvas>
+              <OrbitControls />
+              {/* Render the model when the marker is found */}
+              <Bauhaus />
+              <Environment preset="sunset" />
+            </Canvas>
+          )}
         </a-marker>
         <a-entity camera />
       </a-scene>
