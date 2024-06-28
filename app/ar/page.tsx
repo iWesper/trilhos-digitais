67948016e4cb5a3fc0e0b0b3135e973cced95a40
Luís Costa <luis.pedro10@live.com.pt>
2024-06-28 02:08:56 +0100

    WebXR Testing

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -1,57 +1,13 @@
 "use client";
-import React, { Suspense, useState, useEffect } from "react";
-import { Interactive, ARButton, XR, Controllers } from "@react-three/xr";
-import { Text } from "@react-three/drei";
+
+import React, { useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
-import { BoxGeometry, MeshPhongMaterial, Mesh } from "three";
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
+import { ARButton, XR } from "@react-three/xr";
+import Bauhaus from "@/public/models/bauhaus/Bauhaus";
 
-function Box({ color, size, scale, children, ...rest }: any) {
-  return (
-    <mesh scale={scale} {...rest}>
-      <boxGeometry args={size} />
-      <meshPhongMaterial color={color} />
-      {children}
-    </mesh>
-  );
-}
-
-function Button(props: any) {
-  const [hover, setHover] = useState(false);
-  const [color, setColor] = useState<any>("blue");
-
-  const onSelect = () => {
-    setColor((Math.random() * 0xffffff) | 0);
-  };
-
-  return (
-    <Interactive
-      onHover={() => setHover(true)}
-      onBlur={() => setHover(false)}
-      onSelect={onSelect}
-    >
-      <Box
-        color={color}
-        scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
-        size={[0.4, 0.1, 0.1]}
-        {...props}
-      >
-        <Suspense fallback={null}>
-          <Text
-            position={[0, 0, 0.06]}
-            fontSize={0.05}
-            color="#000"
-            anchorX="center"
-            anchorY="middle"
-          >
-            Hello react-xr!
-          </Text>
-        </Suspense>
-      </Box>
-    </Interactive>
-  );
-}
-
-export default function ARComponent() {
+// Step 4: Main AR Page Component
+export default function ARPage() {
   useEffect(() => {
     // Check for WebXR support
     if (!navigator.xr) {
@@ -61,15 +17,14 @@ export default function ARComponent() {
 
   return (
     <div className="w-screen h-screen">
-      <ARButton />
       <Canvas>
-        <XR referenceSpace="local">
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Button position={[0, 0.1, -0.2]} />
-          <Controllers />
+        <XR>
+          <OrbitControls autoRotate={true} autoRotateSpeed={0.5} />
+          <Bauhaus />
+          <Environment preset="sunset" />
         </XR>
       </Canvas>
+      <ARButton />
     </div>
   );
 }
