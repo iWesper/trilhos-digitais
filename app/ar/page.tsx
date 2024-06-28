52bcfb9b4abcbb4ca734276d78e6d7b82f137669
Luís Costa <luis.pedro10@live.com.pt>
2024-06-28 01:48:12 +0100

    refactor: Update AR.js library and adjust image positioning in Comboio component

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -24,7 +24,7 @@ function Box({ color, size, scale, children, ...rest }: any) {
   
     return (
       <Interactive onHover={() => setHover(true)} onBlur={() => setHover(false)} onSelect={onSelect}>
-        <Box color={color} scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]} size={[0.4, 0.1, 0.1]} {...props}>
+        <Box color={color} scale={hover ? [1.2, 1.2, 1.2] : [1, 1, 1]} size={[1, 0.7, 0.7]} {...props}>
           <Suspense fallback={null}>
             <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
               Hello react-xr!
@@ -40,11 +40,10 @@ export default function ARComponent() {
     <div className='w-screen h-screen'>
       <ARButton />
       <Canvas className='w-screen h-screen'>
-        <XR referenceSpace="local">
+        <XR referenceSpace='local-floor'>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
           <Button position={[0, 0.1, -0.2]} />
-          <Controllers />
         </XR>
       </Canvas>
     </div>
