0fc79da7ad06b74b73f99f6dd96a12963cc61a5e
Luís Costa <luis.pedro10@live.com.pt>
2024-06-28 01:12:34 +0100

    AR testing

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -1,20 +1,52 @@
 'use client'
-import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import React, { Suspense, useState } from 'react'
+import { Interactive, ARButton, XR, Controllers } from '@react-three/xr'
+import { Text } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+
+function Box({ color, size, scale, children, ...rest }: any) {
+    return (
+      <mesh scale={scale} {...rest}>
+        <boxGeometry args={size} />
+        <meshPhongMaterial color={color} />
+        {children}
+      </mesh>
+    )
+  }
+  
+  function Button(props: any) {
+    const [hover, setHover] = useState(false)
+    const [color, setColor] = useState<any>('blue')
+  
+    const onSelect = () => {
+      setColor((Math.random() * 0xffffff) | 0)
+    }
+  
+    return (
+      <Interactive onHover={() => setHover(true)} onBlur={() => setHover(false)} onSelect={onSelect}>
+        <Box color={color} scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]} size={[0.4, 0.1, 0.1]} {...props}>
+          <Suspense fallback={null}>
+            <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
+              Hello react-xr!
+            </Text>
+          </Suspense>
+        </Box>
+      </Interactive>
+    )
+  }
 
 export default function ARComponent() {
   return (
-    <>
+    <div className='w-screen h-screen'>
       <ARButton />
-      <Canvas>
-        <XR>
+      <Canvas className='w-screen h-screen'>
+        <XR referenceSpace="local">
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Button position={[0, 0.1, -0.2]} />
           <Controllers />
-          <mesh>
-            <boxGeometry />
-            <meshBasicMaterial color="blue" />
-          </mesh>
         </XR>
       </Canvas>
-    </>
+    </div>
   )
 }
