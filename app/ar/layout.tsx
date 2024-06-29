fd859e2affcbdded25493991d7f5f59de284bd0b
T0mas Sousa <tomasrafael@ua.pt>
2024-06-29 23:43:50 +0100

    refactor: Update ARLayout component and remove ARPage component

---
+++ app/ar/layout.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useState, Suspense } from "react";
+import { ProgressProvider } from "@/components/context/ProgressContext";
+import { useAuth } from "@/components/context/AuthContext";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import Lottie from "lottie-react";
+import animationData from "@/public/animations/loading_animation.json";
+
+export default function ARLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { currentUser, isLoading } = useAuth();
+  const router = useRouter();
+
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+
+  // Verifica se o utilizador está autenticado
+  useEffect(() => {
+    if (!isLoading) {
+      if (currentUser === null) {
+        setIsCheckingAuth(false);
+        router.push("/authentication");
+      } else {
+        setIsCheckingAuth(false);
+      }
+    }
+  }, [currentUser, isLoading, router]);
+
+  // Se o utilizador estiver a ser autenticado, mostra um loading
+  if (isLoading || isCheckingAuth) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <Lottie
+          animationData={animationData}
+          className="bg-[#142839] h-20 w-20"
+        />
+      </div>
+    );
+  }
+
+  return (
+    <Suspense
+      fallback={
+        <div className="h-screen w-screen flex justify-center items-center">
+          <Lottie
+            animationData={animationData}
+            className="bg-[#142839] h-20 w-20"
+          />
+        </div>
+      }
+    >
+      <ProgressProvider>
+        <main className="overflow-hidden text-sm md:text-md lg:text-xl 2xl:text-2xl 3xl:text-3xl 4xl:text-4xl">
+          {children}
+        </main>
+      </ProgressProvider>
+    </Suspense>
+  );
+}
