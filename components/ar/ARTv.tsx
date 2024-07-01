1a73bbbffe91f6517cd78f4b6730dd11ba8b61f5
Luís Costa <luis.pedro10@live.com.pt>
2024-07-01 14:38:32 +0100

    - New AR Model for TV - New type of interaction: Video player inside the TV. Interact with the TV Model to play the video.

--- components/ar/ARTv.tsx
+++ components/ar/ARTv.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Environment, useGLTF, useAnimations } from "@react-three/drei";
 import { ARButton, Interactive, XR } from "@react-three/xr";
-import { Mesh, MeshStandardMaterial } from "three";
+import { Mesh, MeshStandardMaterial, VideoTexture } from "three";
 
 export default function ARPage() {
   const [isARActive, setIsARActive] = useState(false);
@@ -25,11 +25,22 @@ export default function ARPage() {
 
   return (
     <div className="w-screen h-screen">
+      <video
+        id="video"
+        playsInline
+        webkit-playsinline
+        loop
+        autoPlay
+        width={1920}
+        height={1080}
+        src="/videos/tvar.mp4"
+        style={{ display: "none" }}
+      ></video>
       <Canvas>
         <XR referenceSpace="local">
           {isARActive && (
             <>
-              <TvAR scale={0.0275} position={[0.125, -0.8, -1.25]} />
+              <TvAR scale={0.4} position={[0.125, -0.25, -1.25]} />
               <Environment preset="sunset" />
             </>
           )}
@@ -43,86 +54,43 @@ export default function ARPage() {
 function TvAR(props: any) {
   const group = useRef();
   const { nodes, materials, animations } = useGLTF(
-    "/models/prensaAR/prensaAR-transformed.glb"
+    "/models/tvAR/tvAR-transformed.glb"
   );
-  const { actions } = useAnimations(animations, group);
+  const video = document.getElementById("video") as HTMLVideoElement;
+  const videoTexture = new VideoTexture(video);
+  videoTexture.flipY = false;
 
   // Handler das interações
   const handleInteraction = () => {
-    // Ir buscar o nome das animações
-    const actionNames = Object.keys(actions);
-    // Verificar se existem animações
-    if (actionNames.length > 0) {
-      const firstActionName = actionNames[0];
-      const animationAction = actions[firstActionName];
-      if (animationAction) {
-        if (animationAction.isRunning() && !animationAction.paused) {
-          animationAction.paused = true; // Pausar a animação se estiver a correr
-        } else {
-          animationAction.reset().play(); // Iniciar a animação se não estiver a correr ou pausada
-        }
-      }
+    if (video.paused) {
+      video.play(); // Reproduzir o vídeo se estiver pausado
     } else {
-      return;
+      video.pause(); // Pausar o vídeo se estiver a reproduzir
     }
   };
 
+  useFrame(() => {
+    videoTexture.needsUpdate = true;
+  });
+
   return (
     <Interactive onSelect={handleInteraction}>
-      <group ref={group} {...props} dispose={null}>
-        <group name="Scene">
-          <group
-            name="untitled001"
-            position={[0.073, 0, 0]}
-            rotation={[-Math.PI / 2, 0, 0.009]}
-            scale={0.01}
-          >
-            <mesh
-              name="s__1_001"
-              geometry={(nodes.s__1_001 as Mesh).geometry}
-              material={
-                materials[
-                  "Vintage_Newspaper_Print_Texture_Background_58059866_1.001"
-                ]
-              }
-              position={[-404, 768, 1518]}
-              scale={[9.674, 15.588, 0.229]}
-            />
-          </group>
-          <mesh
-            name="Object_2"
-            geometry={(nodes.Object_2 as Mesh).geometry}
-            material={new MeshStandardMaterial({ color: "#6f4e37" })}
-            rotation={[-Math.PI / 2, 0, 0]}
-          />
-          <mesh
-            name="s001"
-            geometry={(nodes.s001 as Mesh).geometry}
-            material={
-              materials[
-                "DD_Old_Paper_Texture_77722_DD_Old_Paper_Texture_77722.001"
-              ]
-            }
-            position={[-4.043, 15.18, -7.692]}
-            rotation={[-Math.PI / 2, 0, 0.009]}
-            scale={[0.097, 0.156, 0.002]}
-          />
-          <mesh
-            name="s__2_001"
-            geometry={(nodes.s__2_001 as Mesh).geometry}
-            material={
-              materials[
-                "Vintage_Newspaper_Print_Texture_Background_58059866_1.001"
-              ]
-            }
-            position={[-3.92, 14.01, 5.256]}
-            rotation={[-Math.PI / 2, 0, 0.009]}
-            scale={[0.063, 0.101, 0.001]}
-          />
-        </group>
+      <group {...props} dispose={null}>
+        <mesh
+          geometry={(nodes.body as Mesh).geometry}
+          material={materials.PaletteMaterial001}
+          rotation={[Math.PI / 2, 0, 0]}
+          scale={0.004}
+        />
+        <mesh
+          geometry={(nodes.Cube as Mesh).geometry}
+          material={new MeshStandardMaterial({ map: videoTexture })}
+          position={[-0.508, -0.181, 0.058]}
+          scale={[1.009, 0.741, 0.07]}
+        />
       </group>
     </Interactive>
   );
 }
 
-useGLTF.preload("/models/prensaAR/prensaAR-transformed.glb");
+useGLTF.preload("/models/tvAR/tvAR-transformed.glb");
